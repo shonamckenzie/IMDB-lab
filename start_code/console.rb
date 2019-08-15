889eefe559dff5dab79fedf9d5c8066d09106b11
Ian Shepherd <ianshepherd77@gmail.com>
2019-08-15 14:44:47 +0100

    set up save methods

--- start_code/console.rb
+++ start_code/console.rb
@@ -6,6 +6,10 @@ require('pry-byebug')
 
 star1 = Star.new({ 'first_name' => 'Tom', 'last_name' => 'Cruise'})
 star2 = Star.new({ 'first_name' => 'Daniel', 'last_name' => 'Craig'})
+star1.save()
+star2.save()
 
 movie1 = Movie.new({ 'title' => 'Mission Impossible', 'genre' => 'action'})
 movie2 = Movie.new({ 'title' => 'Casino Royale', 'genre' => 'comedy'})
+movie1.save()
+movie2.save()
--- start_code/models/star.rb
+++ start_code/models/star.rb
@@ -1,3 +1,32 @@
+require_relative("../db/sql_runner")
+require_relative("./movie")
+
 class Star
+
+attr_reader :id
+attr_accessor :first_name, :last_name
+
+def initialize( options)
+  @id = options['id'].to_i if options['id']
+  @first_name = options['first_name']
+  @last_name = options['last_name']
+end
+
+def save()
+  sql = "INSERT INTO stars
+(
+first_name,
+last_name
+)
+VALUES
+(
+$1, $2
+)
+RETURNING id"
+values = [@first_name, @last_name]
+star = SqlRunner.run( sql, values ).first
+@id = star['id'].to_i
+end
+
 
 end
